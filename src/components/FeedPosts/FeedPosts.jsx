f60424169d1546113c25af8b3361970872e4ecfa
albert19_ <albertnanda1902@gmail.com>
2023-12-25 20:35:43 +0800

    create slicing for comment input and toast username, image and avatar props

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -4,10 +4,10 @@ import FeedPost from './FeedPost'
 const FeedPosts = () => {
     return (
         <Container maxW={"container.sm"} py={10} px={2}>
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
+            <FeedPost img='/img1.png' username="albert" avatar='/img1.png' />
+            <FeedPost img='/img2.png' username="nanda" avatar='/img2.png' />
+            <FeedPost img='/img3.png' username="dono" avatar='/img3.png' />
+            <FeedPost img='/img4.png' username="andre" avatar='/img4.png' />
         </Container>
     )
 }
