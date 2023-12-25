82363adfe56c988b2a1fc6661624d820a40bbb82
albert19_ <albertnanda1902@gmail.com>
2023-12-25 23:24:05 +0800

    create layout for profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import { Route } from 'react-router-dom'
 import Auth from './pages/Auth'
 import PageLayout from './layout/PageLayout'
+import Profile from './pages/Profile'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/:username" element={<Profile />} />
       </Routes>
     </PageLayout>
   )
