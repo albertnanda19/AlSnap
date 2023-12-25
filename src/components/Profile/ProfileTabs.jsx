84e8e27a8afa0c82f19126602d75dcda5ee6fbd8
albert19_ <albertnanda1902@gmail.com>
2023-12-26 01:18:24 +0800

    create slicing for profile tabs

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,6 +1,17 @@
+import { Flex, Text } from '@chakra-ui/react';
+
 const ProfileTabs = () => {
     return (
-        <div>ProfileTabs</div>
+        <Flex w={"full"} justifyContent={"center"} gap={{ base: 4, sm: 10 }} textTransform={"uppercase"} fontWeight={"bold"}>
+            <Flex borderTop={"1px solid white"} alignItems={"center"} p={"3"} gap={1} cursor={"pointer"}>
+                {/* <Box fontSize={20}>
+                    <BsGrid3X3 />
+                </Box> */}
+                <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+                    Posts
+                </Text>
+            </Flex>
+        </Flex>
     )
 }
 
