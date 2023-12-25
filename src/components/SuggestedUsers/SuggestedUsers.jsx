e2e98e9a60380b3964f6f65abb100db42851eb1f
albert19_ <albertnanda1902@gmail.com>
2023-12-25 23:07:50 +0800

    create slicing ui for suggested users

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,10 +1,32 @@
-import { VStack } from '@chakra-ui/react'
+import { Flex, Text, VStack, Box, Link } from '@chakra-ui/react'
 import SuggestedHeader from './SuggestedHeader'
+import SuggestedUser from './SuggestedUser'
 
 const SuggestedUsers = () => {
     return (
         <VStack py={8} px={6} gap={4}>
             <SuggestedHeader />
+
+            <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+                <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+                    Suggested for you
+                </Text>
+                <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+                    See All
+                </Text>
+            </Flex>
+
+            <SuggestedUser name="Akio" followers={1392} avatar='https://bit.ly/dan-abramov' />
+            <SuggestedUser name="Budi" followers={567} avatar='https://bit.ly/ryan-florence' />
+            <SuggestedUser name="Akia" followers={759} avatar='https://bit.ly/code-beast' />
+
+
+            <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+                © 2023 Built By{"  "}
+                <Link href='/' target='_blank' color={"blue.500"} fontSize={14}>
+                    Albert Mangiri
+                </Link>
+            </Box>
         </VStack>
     )
 }
