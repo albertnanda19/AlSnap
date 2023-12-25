0a77dab50178cf79d5d6168f3a9555f5ed94eae6
albert19_ <albertnanda1902@gmail.com>
2023-12-25 20:57:43 +0800

    intiate suggested users files component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Box, Container, Flex } from "@chakra-ui/react";
 import FeedPosts from "../components/FeedPosts/FeedPosts";
+import SuggestedUsers from "../components/SuggestedUsers/SuggestedUsers";
 
 const Home = () => {
     return (
@@ -9,7 +10,7 @@ const Home = () => {
                     <FeedPosts />
                 </Box>
                 <Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"} >
-                    Suggested Users
+                    <SuggestedUsers />
                 </Box>
             </Flex>
         </Container>
