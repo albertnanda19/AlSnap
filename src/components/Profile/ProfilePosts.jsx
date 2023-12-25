488c78e6ebff84026d30afbb9e97e47dc185992a
albert19_ <albertnanda1902@gmail.com>
2023-12-26 01:41:46 +0800

    create posts layout on profile page

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,6 +1,41 @@
+import { Grid, VStack, Box, Skeleton } from '@chakra-ui/react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import ProfilePost from './ProfilePost'
+
 const ProfilePosts = () => {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 2000)
+    }, [])
+
     return (
-        <div>ProfilePosts</div>
+        <Grid
+            templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
+            gap={1}
+            columnGap={1}
+        >
+            {isLoading && [0, 1, 2, 3, 4, 5, 6].map((_, index) => (
+                <VStack key={index} alignItems={"flex-start"} gap={4}>
+                    <Skeleton w={"full"}>
+                        <Box h={"300px"}>Contents wrapped</Box>
+                    </Skeleton>
+                </VStack>
+            ))}
+
+            {!isLoading && (
+                <>
+                    <ProfilePost img="/img1.png" />
+                    <ProfilePost img="/img2.png" />
+                    <ProfilePost img="/img3.png" />
+                    <ProfilePost img="/img4.png" />
+                </>
+            )}
+        </Grid>
     )
 }
 
