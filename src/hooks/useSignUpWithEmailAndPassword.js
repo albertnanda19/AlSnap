a1c964f9259cb219377a1c711890c98e1d4ff9d6
albert19_ <albertnanda1902@gmail.com>
2023-12-26 16:58:16 +0800

    create error toast

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,26 +1,24 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import { auth, firestore } from '../firebase/firebase'
 import { doc, setDoc } from 'firebase/firestore'
+import useShowToast from './useShowToast';
 
 const useSignUpWithEmailAndPassword = () => {
 
     const [
-        createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+        createUserWithEmailAndPassword, , loading, error] = useCreateUserWithEmailAndPassword(auth);
+    const showToast = useShowToast();
 
     const signup = async (inputs) => {
         if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
-            console.log("Please fill all the fields.");
+            showToast("Error", "Please fill all the fields", "error");
             return;
         }
 
         try {
             const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
             if (!newUser && error) {
-                console.log(error);
+                showToast("Error", error.message, "error");
                 return;
             }
 
@@ -43,7 +41,7 @@ const useSignUpWithEmailAndPassword = () => {
             }
 
         } catch (error) {
-            console.log(error);
+            showToast("Error", error.message, "error");
         }
     }
 
