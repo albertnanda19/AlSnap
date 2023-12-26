e574022fcaf67bea5a05c75954926c450536c9fe
albert19_ <albertnanda1902@gmail.com>
2023-12-26 17:48:55 +0800

    create useStore function using zustand to check user is auth or not

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import { Route } from 'react-router-dom'
 import Auth from './pages/Auth'
 import PageLayout from './layout/PageLayout'
 import Profile from './pages/Profile'
+import useAuthStore from './store/authStore'
+import { Navigate } from 'react-router-dom'
 
 function App() {
+
+  const authUser = useAuthStore(state => state.user);
 
   return (
     <PageLayout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/auth"} />} />
+        <Route path="/auth" element={!authUser ? <Auth /> : <Navigate to={"/"} />} />
         <Route path="/:username" element={<Profile />} />
       </Routes>
     </PageLayout>
