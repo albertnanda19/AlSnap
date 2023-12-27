976129703968aa6c9c36f40b8c38e5e371c4e468
albert19_ <albertnanda1902@gmail.com>
2023-12-28 03:07:59 +0800

    create update user functionality by creating useEditProfile hook

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -19,6 +19,8 @@ import { useState } from "react";
 import useAuthStore from "../../store/authStore";
 import { useRef } from "react";
 import usePreviewImg from "../../hooks/usePreviewImg";
+import useEditProfile from "../../hooks/useEditProfile";
+import useShowToast from "../../hooks/useShowToast";
 
 const EditProfile = ({ isOpen, onClose }) => {
 
@@ -31,9 +33,17 @@ const EditProfile = ({ isOpen, onClose }) => {
     const authUser = useAuthStore((state) => state.user);
     const fileRef = useRef(null)
     const { handleImageChange, selectedFile, setSelectedFile } = usePreviewImg();
+    const { isUpdating, editProfile } = useEditProfile();
+    const showToast = useShowToast();
 
-    const handleEditProfile = () => {
-        console.log(inputs);
+    const handleEditProfile = async () => {
+        try {
+            await editProfile(inputs, selectedFile);
+            setSelectedFile(null);
+            onClose();
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        }
     }
 
     return (
@@ -94,6 +104,7 @@ const EditProfile = ({ isOpen, onClose }) => {
                                         w='full'
                                         _hover={{ bg: "blue.500" }}
                                         onClick={handleEditProfile}
+                                        isLoading={isUpdating}
                                     >
                                         Submit
                                     </Button>
