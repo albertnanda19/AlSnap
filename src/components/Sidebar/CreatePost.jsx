6f98053bd7438598dcced2aba843d856fda1899e
albert19_ <albertnanda1902@gmail.com>
2023-12-29 23:55:50 +0800

    create a function to store post and caption into database

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -1,9 +1,17 @@
-import { Box, Flex, Tooltip, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Input, ModalFooter, Button, Textarea, useDisclosure, Image, CloseButton, Text } from "@chakra-ui/react";
+import { Box, Flex, Tooltip, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Input, ModalFooter, Button, Textarea, useDisclosure, Image, CloseButton } from "@chakra-ui/react";
 import { CreatePostLogo } from "../../assets/constants";
 import { BsFillImageFill } from "react-icons/bs";
 import { useState } from "react";
 import { useRef } from "react";
 import usePreviewImg from "../../hooks/usePreviewImg"
+import useShowToast from "../../hooks/useShowToast";
+import useAuthStore from "../../store/authStore"
+import usePostStore from "../../store/postStore";
+import useUserProfileStore from "../../store/userProfileStore";
+import { useLocation } from "react-router-dom";
+import { addDoc, arrayUnion, collection, doc, updateDoc } from "firebase/firestore";
+import { firestore, storage } from "../../firebase/firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const CreatePost = () => {
 
@@ -11,6 +19,19 @@ const CreatePost = () => {
     const [caption, setCaption] = useState("");
     const imageRef = useRef(null);
     const { handleImageChange, selectedFile, setSelectedFile } = usePreviewImg();
+    const { isLoading, handleCreatePost } = useCreatePost();
+    const showToast = useShowToast();
+
+    const handlePostCreation = async () => {
+        try {
+            await handleCreatePost(selectedFile, caption);
+            onClose();
+            setCaption("");
+            setSelectedFile(null);
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        }
+    }
 
     return (
         <>
@@ -63,8 +84,7 @@ const CreatePost = () => {
                         )}
                     </ModalBody>
                     <ModalFooter>
-                        <Button mr={3}>Post</Button>
-                        <Text color={"white"}>{selectedFile}</Text>
+                        <Button mr={3} onClick={handlePostCreation} isLoading={isLoading}>Post</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
@@ -72,4 +92,50 @@ const CreatePost = () => {
     )
 }
 
-export default CreatePost
+export default CreatePost;
+
+function useCreatePost() {
+    const showToast = useShowToast();
+    const [isLoading, setIsLoading] = useState(false);
+    const authUser = useAuthStore((state) => state.user);
+    const createPost = usePostStore((state) => state.createPost);
+    const addPost = useUserProfileStore(state => state.addPost);
+    const { pathname } = useLocation();
+
+    const handleCreatePost = async (selectedFile, caption) => {
+        if (isLoading) return;
+        if (!selectedFile) throw new Error("Please select an image");
+        setIsLoading(true);
+
+        const newPost = {
+            caption: caption,
+            likes: [],
+            comments: [],
+            createdAt: Date.now(),
+            createdBy: authUser.uid,
+        }
+
+        try {
+            const postDocRef = await addDoc(collection(firestore, "posts"), newPost);
+            const userDocRef = doc(firestore, "users", authUser.uid);
+            const imageRef = ref(storage, `posts/${postDocRef.id}`);
+            await updateDoc(userDocRef, { posts: arrayUnion(postDocRef.id) });
+            await uploadString(imageRef, selectedFile, "data_url");
+            const downloadURL = await getDownloadURL(imageRef);
+            await updateDoc(postDocRef, { imageURL: downloadURL });
+
+            newPost.imageURL = downloadURL;
+            createPost({ ...newPost, id: postDocRef.id });
+
+            addPost({ ...newPost, id: postDocRef.id });
+            showToast("Success", "Post created sucessfully", "success");
+
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return { isLoading, handleCreatePost };
+}
