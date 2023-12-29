b2eaa92fec7c8543cca882183f10121e3875f448
albert19_ <albertnanda1902@gmail.com>
2023-12-29 18:43:15 +0800

    create modal for create post and preview post image

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -1,7 +1,17 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { Box, Flex, Tooltip, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Input, ModalFooter, Button, Textarea, useDisclosure, Image, CloseButton, Text } from "@chakra-ui/react";
 import { CreatePostLogo } from "../../assets/constants";
+import { BsFillImageFill } from "react-icons/bs";
+import { useState } from "react";
+import { useRef } from "react";
+import usePreviewImg from "../../hooks/usePreviewImg"
 
 const CreatePost = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [caption, setCaption] = useState("");
+    const imageRef = useRef(null);
+    const { handleImageChange, selectedFile, setSelectedFile } = usePreviewImg();
+
     return (
         <>
             <Tooltip
@@ -20,11 +30,44 @@ const CreatePost = () => {
                     p={2}
                     w={{ base: 10, md: "full" }}
                     justifyContent={{ base: "center", md: "flex-start" }}
+                    onClick={onOpen}
                 >
                     <CreatePostLogo />
                     <Box display={{ base: "none", md: "block" }}>Create</Box>
                 </Flex>
             </Tooltip>
+
+            <Modal isOpen={isOpen} onClose={onClose} size={"xl"}>
+                <ModalOverlay />
+
+                <ModalContent bg={"black"} border={"1px solid gray"}>
+                    <ModalHeader>Create Post</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <Textarea placeholder="Post caption ..." value={caption} onChange={(e) => setCaption(e.target.value)} />
+
+                        <Input type="file" hidden ref={imageRef} onChange={handleImageChange} />
+
+                        <BsFillImageFill style={{ marginTop: "15px", marginLeft: "5px", cursor: "pointer" }} size={16} onClick={() => imageRef.current.click()} />
+                        {selectedFile && (
+                            <Flex mt={5} w={"full"} position={"relative"} justifyContent={"center"}>
+                                <Image src={selectedFile} alt="Selected Img" />
+                                <CloseButton
+                                    position={"absolute"}
+                                    top={2}
+                                    right={2}
+                                    onClick={() => setSelectedFile("")}
+                                >
+                                </CloseButton>
+                            </Flex>
+                        )}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button mr={3}>Post</Button>
+                        <Text color={"white"}>{selectedFile}</Text>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     )
 }
