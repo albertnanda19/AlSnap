bcef4f1eae8ca629fbc20cc24cb7bf0eb295c8a1
albert19_ <albertnanda1902@gmail.com>
2023-12-30 19:59:19 +0800

    create CommentSkeleton component

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,6 +1,11 @@
-import { Avatar, Flex, Text } from '@chakra-ui/react'
+import { Avatar, Flex, Text, SkeletonCircle, Skeleton } from '@chakra-ui/react'
+import useGetUserProfileById from '../../hooks/useGetUserProfileById'
 
 const Comment = ({ comment }) => {
+
+    const { userProfile, isLoading } = useGetUserProfileById(comment.createdBy);
+    if (isLoading) return <CommentSkeleton />
+
     return (
         <Flex gap={4}>
             {/* <Avatar src={profilePic} name={username} size={'sm'} /> */}
@@ -20,3 +25,15 @@ const Comment = ({ comment }) => {
 }
 
 export default Comment
+
+const CommentSkeleton = () => {
+    return (
+        <Flex gap={4} w={"full"} alignItems={"center"}>
+            <SkeletonCircle h={10} w={10} />
+            <Flex gap={1} flexDir={"column"}>
+                <Skeleton height={2} width={100} />
+                <Skeleton height={2} width={50} />
+            </Flex>
+        </Flex>
+    )
+}
